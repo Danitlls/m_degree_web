e3678852e609c7a005564ef77805ada6e8113619
Danitlls <danielatrulls01@gmail.com>
2018-07-13 09:39:57 -0700

    Final container for home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,20 +7,21 @@ export default function Home() {
     <div className="home-container">
       <div>
         <div className="logo-animable-container">
-          <img className="logo-animable" src="/img/second-logo1.svg" />
+          <img className="logo-animable" src="/img/MDegree_LogoTag_Final.png" />
         </div>
       </div>
 
-      <div className="row home-desserts-container">
+      <div className="row mx-5 mt-2 home-desserts-container">
+
         <div className="card-mm  col s12 m6">
           <Link to='/about'>
             <div className="image-container" >
               <img src="/img/market-home.JPG" />
-
             </div>
             <span>About & Contact Us</span>
           </Link>
         </div>
+
         <div className="card-mm col s12 m6">
           <Link to='/press'>
             <div className="image-container" >
@@ -29,10 +30,8 @@ export default function Home() {
             <span>Press & Collaborations</span>
           </Link>
         </div>
-      </div>
-      <div className="row home-desserts-container">
         <Link to='/cakes'>
-          <div className="card-mm col s12 m4">
+          <div className="card-mm col s12 m6 l4">
             <div className="image-container" >
               <img src="/img/cakes02small.jpg" />
             </div>
@@ -40,7 +39,7 @@ export default function Home() {
           </div>
         </Link>
         <Link to='/plated'>
-          <div className="card-mm  col s12 m4">
+          <div className="card-mm  col s12 m6 l4">
             <div className="image-container" >
               <img src="/img/plated/DSCF1887.jpg" />
             </div>
@@ -48,7 +47,7 @@ export default function Home() {
           </div>
         </Link>
         <Link to='/baked'>
-          <div className="card-mm  col s12 m4">
+          <div className="card-mm  col m12 l4">
             <div className="image-container" >
               <img src="/img/baked/croi1.jpg" />
             </div>
