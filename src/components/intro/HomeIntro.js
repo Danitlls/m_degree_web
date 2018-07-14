9004c0026d8f5a64f0ea160f2ad9404a5c96bfe8
Danitlls <danielatrulls01@gmail.com>
2018-07-13 17:13:01 -0700

    Fix margin imageGrid

--- src/components/intro/HomeIntro.js
+++ src/components/intro/HomeIntro.js
@@ -5,10 +5,12 @@ export default function HomeIntro({ children }) {
   return (
     <div>
       {children}
-      <div className="full-back" />
-      <div className="logo-into">
-    <img  src="/img/mdegree_gif.gif" />
+      <div className="full-back">
+        <div className="logo-into">
+          // <img  src="/img/mdegree_gif.gif" />
+          <img  src="/img/MDegree_LogoTag_Final_small.png" />
+        </div>
+      </div>
     </div>
-  </div>
   );
 }
